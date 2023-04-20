7437d45ee56bc23e5041293ffa9b7c853c426561
Amaroq <nike-marie@hotmail.de>
2023-04-20 17:08:45 +0200

    Update A1.2.py
    comments need to be finished, as well as analyzing the runtime.

--- HA1/A1.2.py
+++ HA1/A1.2.py
@@ -12,20 +12,40 @@ def swap(l,i,j):
 
 def quicksort(l):
     '''
-    sort given list with algorithm quicksort.
+    sort given list with algorithm quicksort using three-way partitioning.
     '''
-
     def qsort_h(start,end):
-        if start+1 < end:
+        if start < end:
+            '''
+            define pivot first using a random index within the list
+            then initialize less-than (lt) and greater-than (gt) as markers for partitions
+            initialize m as counting variable
+            '''
+            pivot = l[random.randint(start, end -1)]
+            lt = start
+            gt = end
             m = start + 1
-            swap(l,start,random.randint(start,end-1))
-            for i in range(m,end):
-                if l[i] < l[start]:
-                    swap(l,m,i)
-                    m = m + 1
-            swap(l,start,m-1) # put pivot in the middle
-            qsort_h(start,m-1)
-            qsort_h(m,end)
+            while m < gt:
+                '''
+                in the beginning, m cannot be equal or larger than gt, unless the list contains 0 or 1 element
+                if that were the case, the list will be returned as is
+                ''' 
+                if l[m] < pivot:
+                    '''
+                    if the current list-item is smaller than the pivot point, it already is on the correct side of the pivot
+                    so it can be put in the less-than partition
+                    in that case the lt-partition grows by one item, so the lt-marker moves to the right
+                    '''
+                    swap(l, m, lt)
+                    m += 1
+                    lt += 1
+                elif l[m] > pivot:
+                    gt -= 1
+                    swap(l, m, gt)
+                else:
+                    m += 1
+                qsort_h(start, lt)
+                qsort_h(gt, end)
 
     qsort_h(0,len(l))
 
@@ -38,7 +58,8 @@ def randlist(n) :
         l.append(i) #random.randint(1,n*100))
     return l
 
-l = [42] * 42
+
+l = [42] * 42 
 print(l)
 quicksort(l)
 print(l)
