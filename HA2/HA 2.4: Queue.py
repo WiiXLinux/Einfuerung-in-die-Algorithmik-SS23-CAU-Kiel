66e8a561cabde195f74f9ce80a2bfcb6aff9e5bc
Amaroq <nike-marie@hotmail.de>
2023-05-01 16:43:10 +0200

    Update HA 2.4: Queue.py
    added dict functions, as well as a brief discussion on how to make the runtime of unenqueue() more efficient

--- HA2/HA 2.4: Queue.py
+++ HA2/HA 2.4: Queue.py
@@ -1,13 +1,29 @@
 '''
 assignment 2.4: Queue
 
-implementation of new methods using classes:
-    list_to_queue() ;;; lines 50 - 55
-    queue_to_list() ;;; lines 58 - 68
-    undequeue() ;;; lines 71 - 78
-    unequeue() ;;; lines 82 - 102
+implementation of new methods using     classes:        dicts:
+    list_to_queue() ;;;                 70 - 75         164 - 170
+    queue_to_list() ;;;                 77 - 87         172 - 180                      
+    undequeue() ;;;                     89 - 97         182 - 190
+    unequeue() ;;;                      99 - 119        192 - 209
 
+More efficient runtime on unequeue() can be achieved if the way the queue is 
+constructed allowed for keeping track of how many items are waiting. The way 
+unenqueue() is currently implemented is not efficient: the program needs to 
+iterate over every element of the queue twice in order to remove the element 
+in the end.
+One of those those passes could be avoided if there was an element-counter
+attribute within the class that is increased or decreased depending on the 
+operation and runtime can be reduced to O(n), n being the number of elements
+currently waiting in queue.
+It may also be possible to implement a queue using nested lists. That way
+it would be quite easy to remove the element at the end of the list by 
+utilizing negative indexing (-1 accesses the last item in the list).
 '''
+
+########
+#USING CLASSES
+########
 
 class Queue():
 
@@ -101,3 +117,93 @@ class Queue():
             current = current['next']
             currentcount -= 1
         current['next'] = {}
+
+######
+#USING DICTS
+######
+
+def empty_queue():
+    '''
+    constructs an empty queue
+    '''
+    empty_elem = {}
+    return {'head' : empty_elem, 'end' : empty_elem}
+
+def enqueue(value,queue):
+    '''
+    adds value to queue (FIFO-Order)
+    '''
+    old_end = queue['end']
+    old_end['value'] = value
+    empty_elem = {}
+    old_end['next'] = empty_elem
+    queue['end'] = empty_elem
+    
+def top(queue):
+    '''
+    returns value of oldest element in the queue, without removing
+    returns None if queue is empty
+    '''
+    if queue['head'] == {}:
+        return None
+    else:
+        return queue['head']['value']
+       
+def dequeue(queue):
+    '''
+    removes oldest element from the queue and returns its value
+    returns None if queue is empty
+    '''
+    if queue['head'] == {}:
+        return None
+    else:
+        result = queue['head']['value']
+        queue['head'] = queue['head']['next']
+        return result
+
+def list_to_queue(queue, list):
+    '''
+    turns a given list into a queue
+    '''
+    for elem in list:
+        enqueue(elem, queue)
+    return queue
+
+def queue_to_list(queue, list):
+    '''
+    turns a queue into list
+    '''
+    while top(queue) != None:
+        list += [top(queue)]
+        dequeue(queue)
+    if top(queue) == None:
+        return (queue, list)
+
+def undequeue(value, queue):
+    '''
+    adds an element to the front of the queue
+    '''
+    old_head = queue['head']
+    empty_elem = {}
+    empty_elem['value'] = value
+    empty_elem['next'] = old_head
+    queue['head'] = empty_elem
+
+def unenqueue(queue):
+    #count how many elements there are in the queue, iterating using current as mutable 
+    #object and currentcount as counter of how many steps the program needs to make to 
+    #remove the last element
+    current = queue['head']
+    currentcount = 0
+    while current['next'] != queue['end']:
+        #count for as many times as there are elements that are not the end of the queue
+        current = current['next']
+        currentcount += 1
+    #reset current to its default state at the beginning of the queue, so we can count 
+    #into the queue again, in order to stop on element before the end. this is the 
+    #element we want to remove
+    current = queue['head']
+    while currentcount > 1:
+        current = current['next']
+        currentcount -= 1
+    current['next'] = {}
