fd5c2463b0a9a6fa796e4d476d1c9f29f8720b32
Henri Heyden <henri@heyden5.de>
2023-05-04 12:16:51 +0200

    Added A1.1

---
+++ HA3/A1.1.py
@@ -0,0 +1,93 @@
+import copy
+
+
+class stack:
+    def __init__(self):
+        """
+        Creates an empty stack.
+        An empty stack doesn't have a left neighbor, and it's Top of Stack is empty.
+        A list representing the same idea would be: [], as you can see there isn't any ToS or neighbor.
+        """
+        self.left = None
+        self.tos = None
+
+    def push(self, obj):
+        """
+        Pushes an object on to the stack.
+        The new tos will have to be adjusted.
+        Because we still need the old stack, we store the old stack as the stack left to the ToS.
+        :param obj: Object to be pushed on the stack
+        :return:
+        """
+        self.left = copy.copy(self)
+        self.tos = obj
+
+    def top(self):
+        """
+        Simple getter for the Top element.
+        :return: Top of Stack
+        """
+        return self.tos
+
+    def pop(self):
+        """
+        Returns the Top of Stack and removes it from the stack.
+        :return: Top of Stack
+        """
+        # Detects that the current stack is empty because it's the same as the initialised stack.
+        if self.left is None:
+            # Notify the user
+            raise Exception("Stack Empty")
+
+        # Because the top of stack can be anything we'll copy it, because it could be an object that isn't a basic type.
+        old_tos = copy.copy(self.tos)
+        # The left half is definitely a stack, so we have to copy it.
+        old_left = copy.copy(self.left)
+
+        # Here we don't have to copy since we're not using the original stack anymore.
+        self.left = self.left.left
+        # If we didn't have copied the old left and just used self.left.tos, then we would be using the ToS one layer
+        # too deep, which would remove one element of the stack.
+        self.tos = old_left.tos
+
+        return old_tos
+
+    def __str__(self):
+        """
+        Not required for the assigment, but useful to debug the code.
+        Converts the stack into a list and then converts this list into a string.
+        We reversed the list because then we can read the right element as ToS.
+        :return: String representation of the stack.
+        """
+        string = []
+        looker = copy.copy(self)
+        while looker.left is not None:
+            string += [looker.tos]
+            looker = looker.left
+        string.reverse()
+        return str(string)
+
+
+s = stack()
+s.push("A")
+s.push("B")
+s.push("C")
+s.push("D")
+s.push("E")
+
+print(s.pop())  # -> E
+print(s.pop())  # -> D
+
+s.push("F")
+s.push("G")
+
+print(s.pop())  # -> G
+print(s.pop())  # -> F
+print(s.pop())
+print(s.pop())
+print(s.pop())
+# Add another pop to demonstrate what happens when the stack is empty.
+try:
+    print(s.pop())
+except Exception as exception:
+    print(exception)
