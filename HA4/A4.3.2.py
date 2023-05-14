0b9f8ff777bb75e887c7fe66fff8d17ebb4c6e81
Henri Heyden <henri@heyden5.de>
2023-05-14 22:20:33 +0200

    Merge to master SEHR WICHTIG: TODO: Comments and remove debugging.

--- HA4/A4.3.2.py
+++ HA4/A4.3.2.py
@@ -1,3 +1,6 @@
+import copy
+
+# Sorry, aber ich kann listen zum Debuggen besser lesen als die stacks aus der vorlesung, weil dessen str methode scheiße ist.
 from stack import Stack
 
 
@@ -21,7 +24,7 @@ def blablub(n):
 
     res = None  # r
     while not stack == []:
-        #print(stack)
+        print("beginning\t\t", stack)
         fun, missing_args, args = stack.pop()
         assert missing_args == 0
 
@@ -35,16 +38,21 @@ def blablub(n):
                 stack.append(('bla', 0, [n - 1]))
                 res = None
         elif fun == 'blub':
-            # n = args[0]
+            n = args[0] # Das war richtig
             if n == 0:
                 res = args[1]
             else:
                 stack.append(('+', 1, []))
-                stack.append(('blub', 0, [res, n - 1]))
+                stack.append(('blub', 0, [args[0] - 1, args[1]])) # Hier war die Reihenfolge, glaube ich anders und res statt args[1] machte auch keinen sinn, weil das ja nicht gilt.
+                res = None # Das hattest du vergessen
         elif fun == '*':
             res = 2 * args[0]
         elif fun == '+':
             res = 1 + args[0]
+
+        print("second\t\t\t", stack, res)
+        c = copy.deepcopy(stack)
+        r = copy.copy(res)
 
         if res != None:
             if not stack == []:
@@ -56,8 +64,11 @@ def blablub(n):
                     stack.append((pfun, missing_args - 1, args + [res]))
                     stack.append(next_call)
 
+        if not c == stack or not r == res:
+            print("after third\t\t", stack, res)
+
     return res
 
 
-for i in range(10):
-    print(bla(i), blablub(i))
+print(blablub(3), bla(3))
+#for i in range(10): print(bla(i), blablub(i))
